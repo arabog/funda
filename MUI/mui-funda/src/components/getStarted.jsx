09da370b76b30a0407c5632388b45e360eacdc2b
arabambi <arababs2015@gmail.com>
2022-03-23 15:48:01 +0100

    MUI getting started done

---
+++ MUI/mui-funda/src/components/getStarted.jsx
@@ -0,0 +1,53 @@
+/*
+npm install @mui/material @emotion/react @emotion/styled
+
+MUI is using emotion as a styling engine by default. If you want 
+to use styled-components instead, run:
+
+// with npm
+npm install @mui/material @mui/styled-engine-sc styled-components
+
+Roboto font
+MUI was designed with the Roboto font in mind. 
+
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>
+Install with npm
+You can install it by typing the below command in your terminal:
+
+npm install @fontsource/roboto
+
+Font icons
+To use the font Icon component, you must first add the Material icons font. 
+
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+
+Installation
+Install the package in your project directory with:
+
+// with npm
+npm install @mui/icons-material
+
+Usage
+Import icons using one of these two options:
+
+Option 1:
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+
+Option 2:
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+
+
+SVG icons
+In order to use prebuilt SVG Material icons, such as those found in the icons 
+demos you must first install the @mui/icons-material package:
+
+// with npm
+npm install @mui/icons-material
+*?
