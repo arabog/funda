aeb2d9c990d79d34910529be5162ebc197ba34e7
arabambi <arababs2015@gmail.com>
2022-03-23 14:39:51 +0100

    React Doc 2

---
+++ React/react-funda/src/components/introJsx.jsx
@@ -0,0 +1,17 @@
+// jsx
+const name = "John";
+const element = <h1>Hello, {name}</h1>;
+
+/*
+Compoent contains both logic and markup
+You can put any valid JavaScript expression inside the curly braces in JSX
+
+JSX is an Expression Too
+After compilation, JSX expressions become regular JavaScript function 
+calls and evaluate to JavaScript objects.
+This means that you can use JSX inside of if statements and for loops, 
+assign it to variables, accept it as arguments, and return it from functions:
+
+
+
+*/ 
