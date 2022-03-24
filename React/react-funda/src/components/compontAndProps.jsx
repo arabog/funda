51d76cc80b05eb7a30f1122b5ee11fc07cdf96b0
arabambi <arababs2015@gmail.com>
2022-03-24 20:38:23 +0100

    comp&props 2

--- React/react-funda/src/components/compontAndProps.jsx
+++ React/react-funda/src/components/compontAndProps.jsx
@@ -104,22 +104,49 @@ For example, consider this Comment component:
 
 function Comment(props) {
           return (
-            <div className="Comment">
-              <div className="UserInfo">
-                <img className="Avatar"
-                  src={props.author.avatarUrl}
-                  alt={props.author.name}
-                />
-                <div className="UserInfo-name">
-                  {props.author.name}
-                </div>
-              </div>
-              <div className="Comment-text">
-                {props.text}
-              </div>
-              <div className="Comment-date">
-                {formatDate(props.date)}
-              </div>
-            </div>
+                    <div className="Comment">
+                              <div className="UserInfo">
+                                        <img className="Avatar"
+                                                  src={props.author.avatarUrl}
+                                                  alt={props.author.name}
+                                        />
+
+                                        <div className="UserInfo-name">
+                                                  {props.author.name}
+                                        </div>
+                              </div>
+
+                              <div className="Comment-text">
+                                        {props.text}
+                              </div>
+
+                              <div className="Comment-date">
+                                        {formatDate(props.date)}
+                              </div>
+                    </div>
           );
-        }
+}
+
+function formatDate(date) {
+          return date.toLocaleDateString();
+}
+
+const comment = {
+          date: new Date(),
+          text: 'I hope you enjoy learning React!',
+
+          author: {
+                    name: 'Hello Kitty',
+                    avatarUrl: 'http://placekitten.com/g/64/64'
+          }
+};
+
+ReactDOM.render(
+          <Comment
+                    date={comment.date}
+                    text={comment.text}
+                    author={comment.author} 
+          />,
+
+          document.getElementById('root')
+);
