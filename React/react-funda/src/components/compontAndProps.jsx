b8a32ec32046d160c7b06f542f76fc26e3ddc351
arabambi <arababs2015@gmail.com>
2022-03-24 20:30:20 +0100

    comp&props 1

--- React/react-funda/src/components/compontAndProps.jsx
+++ React/react-funda/src/components/compontAndProps.jsx
@@ -3,10 +3,10 @@ Components and Props
 Components let you split the UI into independent, reusable pieces, 
 and think about each piece in isolation. 
 
-
 Conceptually, components are like JavaScript functions. They accept 
 arbitrary inputs (called “props”) and return React elements describing 
 what should appear on the screen.
+
 
 Function and Class Components
 The simplest way to define a component is to write a JavaScript function:
@@ -32,6 +32,7 @@ The above two components are equivalent from React’s point of view.
 
 */
 
+
 Rendering a Component
 Previously, we only encountered React elements that represent DOM tags:
 const element = <div />;
@@ -56,7 +57,6 @@ ReactDOM.render(
           document.getElementById('root')
 );
 
-
 Let’s recap what happens in this example:
 1. We call ReactDOM.render() with the <Welcome name="Sara" /> element.
 2. React calls the Welcome component with {name: 'Sara'} as the props.
@@ -69,3 +69,57 @@ For example, <div /> represents an HTML div tag, but <Welcome />
 represents a component and requires Welcome to be in scope.
 
 
+Composing Components
+Components can refer to other components in their output. This lets 
+us use the same component abstraction for any level of detail. 
+A button, a form, a dialog, a screen: in React apps, all those are 
+commonly expressed as components.
+
+For example, we can create an App component that renders Welcome many times:
+
+function Welcome(props) {
+          return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+          return (
+                    <div>
+                              <Welcome name="Sara" />
+                              <Welcome name="Cahal" />
+                              <Welcome name="Edite" />
+                    </div>
+          );
+}
+
+ReactDOM.render(
+          <App />,
+          document.getElementById('root')
+);
+
+
+Extracting Components
+Don’t be afraid to split components into smaller components.
+
+For example, consider this Comment component:
+
+function Comment(props) {
+          return (
+            <div className="Comment">
+              <div className="UserInfo">
+                <img className="Avatar"
+                  src={props.author.avatarUrl}
+                  alt={props.author.name}
+                />
+                <div className="UserInfo-name">
+                  {props.author.name}
+                </div>
+              </div>
+              <div className="Comment-text">
+                {props.text}
+              </div>
+              <div className="Comment-date">
+                {formatDate(props.date)}
+              </div>
+            </div>
+          );
+        }
