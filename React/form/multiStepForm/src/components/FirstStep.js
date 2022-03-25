34ec3496252ded93c42f3f457aa8877ab2e47e3a
arabambi <arababs2015@gmail.com>
2022-03-25 21:32:17 +0100

    FirstStep.js created

---
+++ React/form/multiStepForm/src/components/FirstStep.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const FirstStep = () => {
+          return (
+                    <div>
+                              First Step Form
+                    </div>
+          )
+};
+
+
+export default FirstStep;
