08592c479e7b47bd13e4c165e64cfc8b33aaa4cf
arabambi <arababs2015@gmail.com>
2022-03-31 21:54:31 +0100

    thinking in react 2

--- React/react-funda/src/mainConcept/ThinkingInReact.jsx
+++ React/react-funda/src/mainConcept/ThinkingInReact.jsx
@@ -59,5 +59,110 @@ your UI is updated and where to make changes. React’s
 one-way data flow (also called one-way binding) keeps 
 everything modular and fast.
 
+Step 3: Identify The Minimal (but complete) Representation Of UI State
+To make your UI interactive, you need to be able to trigger 
+changes to your underlying data model. React achieves this 
+with state.
+
+To build your app correctly, you first need to think of the minimal 
+set of mutable state that your app needs. The key here is 
+DRY: Don’t Repeat Yourself. Figure out the absolute minimal 
+representation of the state your application needs and compute 
+everything else you need on-demand. For example, if you’re 
+building a TODO list, keep an array of the TODO items around; 
+don’t keep a separate state variable for the count. Instead, when 
+you want to render the TODO count, take the length of the 
+TODO items array.
+
+Think of all the pieces of data in our example application. We have:
+
+The original list of products
+The search text the user has entered
+The value of the checkbox
+The filtered list of products
+
+Let’s go through each one and figure out which one is state. 
+Ask three questions about each piece of data:
+
+1. Is it passed in from a parent via props? If so, it probably isn’t state.
+2. Does it remain unchanged over time? If so, it probably isn’t state.
+3. Can you compute it based on any other state or props in your 
+component? If so, it isn’t state.
+
+
+The original list of products is passed in as props, so that’s 
+not state. The search text and the checkbox seem to be state 
+since they change over time and can’t be computed from 
+anything. And finally, the filtered list of products isn’t state 
+because it can be computed by combining the original list of 
+products with the search text and value of the checkbox.
+
+So finally, our state is:
+The search text the user has entered
+The value of the checkbox
+
+Step 4: Identify Where Your State Should Live
+OK, so we’ve identified what the minimal set of app state 
+is. Next, we need to identify which component mutates, 
+or owns, this state.
+
+
+For each piece of state in your application:
+
+Identify every component that renders something based on that state.
+
+Find a common owner component (a single component above all 
+the components that need the state in the hierarchy).
+
+Either the common owner or another component higher up in the 
+hierarchy should own the state.
+
+If you can’t find a component where it makes sense to own the 
+state, create a new component solely for holding the state and 
+add it somewhere in the hierarchy above the common owner 
+component.
+
+Let’s run through this strategy for our application:
+ProductTable needs to filter the product list based on state and 
+SearchBar needs to display the search text and checked state.
+
+The common owner component is FilterableProductTable.
+
+It conceptually makes sense for the filter text and checked 
+value to live in FilterableProductTable
+
+
+Cool, so we've decided that our state lives in FilterableProductTable. 
+First, add an instance property this.state = {filterText: '', inStockOnly: false} 
+to FilterableProductTable's constructor to reflect the initial state of your 
+application. Then, pass filterText and inStockOnly to ProductTable and 
+SearchBar as a prop. Finally, use these props to filter the rows in 
+ProductTable and set the values of the form fields in SearchBar.
+
+
+Step 5: Add Inverse Data Flow
+So far, we’ve built an app that renders correctly as a function 
+of props and state flowing down the hierarchy. Now it’s time to 
+support data flowing the other way: the form components deep 
+in the hierarchy need to update the state in FilterableProductTable.
+
+React makes this data flow explicit to help you understand how 
+your program works, but it does require a little more typing than 
+traditional two-way data binding.
+
+If you try to type or check the box in the previous version of the 
+example (step 4), you’ll see that React ignores your input. This 
+is intentional, as we’ve set the value prop of the input to always 
+be equal to the state passed in from FilterableProductTable.
+
+Let’s think about what we want to happen. We want to make 
+sure that whenever the user changes the form, we update the 
+state to reflect the user input. Since components should only 
+update their own state, FilterableProductTable will pass callbacks 
+to SearchBar that will fire whenever the state should be updated. 
+We can use the onChange event on the inputs to be notified of it. 
+he callbacks passed by FilterableProductTable will call setState(), 
+and the app will be updated.
 
 */ 
+
