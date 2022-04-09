99b8c28a4554f998d5705db922942c332c7eea3d
arabambi <arababs2015@gmail.com>
2022-04-09 14:33:34 +0100

    named exports

--- React/react-funda/src/advancedGuides/CodeSplitting.jsx
+++ React/react-funda/src/advancedGuides/CodeSplitting.jsx
@@ -1,5 +1,4 @@
-// /*
-
+/*
 Code-Splitting
 Bundling
 Most React apps will have their files “bundled” using tools 
@@ -249,3 +248,22 @@ const App = () => (
 );
 
 
+Named Exports
+React.lazy currently only supports default exports. If the module you 
+want to import uses named exports, you can create an intermediate 
+module that reexports it as the default. This ensures that tree shaking 
+keeps working and that you don’t pull in unused components.
+
+// ManyComponents.js
+export const MyComponent = /* ... /;
+export const MyUnusedComponent = /* ... /;
+
+// MyComponent.js
+export { MyComponent as default } from "./ManyComponents.js";
+
+// MyApp.js
+import React, { lazy } from 'react';
+const MyComponent = lazy(() => import("./MyComponent.js"));
+
+
+*/
