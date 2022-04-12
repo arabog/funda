8cedbc424446dcb496e88d698303b97348cab5aa
arabambi <arababs2015@gmail.com>
2022-04-12 08:46:03 +0100

    error boundary2

--- React/react-funda/src/advancedGuides/errorBoundaries/index.jsx
+++ React/react-funda/src/advancedGuides/errorBoundaries/index.jsx
@@ -10,8 +10,111 @@ Using try/catch block in each component
 Using React Error Boundary which is only available in 
 class Component
 
+import React from 'react'
 
 
+const City = ({name}) => {
+          return (
+                    <div>
+                              Hello, visit {name.toUpperCase()}
+                    </div>
+          )
+}
+
+
+const Country = ({capital}) => {
+          return (
+                    <div>
+                              Hello, visit {capital.toUpperCase()}
+                    </div>
+          )
+}
+
+
+const TryAndCatch = () => {
+
+
+          return (
+                    <div>
+                              <Country />
+                              <City />          
+                    </div>
+          )
+}
+
+
+export default TryAndCatch
+
+
+Obviously, the error we created in the code above could have 
+certainly been handled with PropTypes or TypeScript, however 
+we are aware runtime error happens all the time and we are going 
+to deal with it using the two approaches stated above.
+
+import React from 'react'
+
+const ErrorHandler =({error}) => {
+          return (
+                    <div role='alert'>
+                              <p>An error occurred:</p>
+
+                              <pre> {error.message} </pre>          
+                    </div>
+          )
+}
+
+
+const City = ({name}) => {
+          try {
+                    return (
+                              <div>
+                                        Hello, visit {name.toUpperCase()}
+                              </div>
+                    )
+                    
+          } catch (err) {
+                    return <ErrorHandler error={err} />
+          }
+}
+
+
+const Country = ({capital}) => {
+          try {
+                    return (
+                              <div>
+                                        Hello, visit {capital.toUpperCase()}
+                              </div>
+                    )
+                    
+          } catch (err) {
+                    return <ErrorHandler error={err} />
+          }
+}
+
+
+const App = () => {
+
+
+          return (
+                    <div>
+                              <Country />
+                              <City />          
+                    </div>
+          )
+}
+
+
+export default App
+
+
+This approach, requires us to define an ErrorHandler component to 
+display in case an error occurs and we wrap each component returned 
+element in the try/catch block.
+
+This seems ok, but repetitive. What if we want the parent component 
+to handle the error catching for us. Wrapping the parent component 
+App in a try/catch block will not work, due to the nature of how React 
+calls functions. That is when React Error Boundary comes in.
 
 
 
