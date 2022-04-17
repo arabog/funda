bb86761f185eeba19de8a2640fe6de1f127820cf
arabambi <arababs2015@gmail.com>
2022-04-17 16:13:29 +0100

    jquery example.jsx added

---
+++ React/react-funda/src/advancedGuides/OtherLib/JQuery/Example.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Chosen from './Chosen'
+
+
+const Example = () => {
+
+
+          return (
+                    <Chosen>
+                              <option>vanilla</option>
+                              <option>chocolate</option>
+                              <option>strawberry</option>
+                    </Chosen>
+          )
+}
+
+
+export default Example
