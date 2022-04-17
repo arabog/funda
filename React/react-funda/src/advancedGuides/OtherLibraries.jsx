0f8e1af9f229e72470ff67fd13bcb9348761cb77
arabambi <arababs2015@gmail.com>
2022-04-17 16:05:43 +0100

    react and oda lib 4

--- React/react-funda/src/advancedGuides/OtherLibraries.jsx
+++ React/react-funda/src/advancedGuides/OtherLibraries.jsx
@@ -181,6 +181,41 @@ This way, Chosen will know to update its DOM element when the
 
 The complete implementation of the Chosen component looks like this:
 
+class Chosen extends React.Component {
+          componentDidMount() {
+                    this.$el = $(this.el);
+                    this.$el.chosen();
+
+                    this.handleChange = this.handleChange.bind(this);
+                    this.$el.on('change', this.handleChange);
+          }
+
+          componentDidUpdate(prevProps) {
+                    if (prevProps.children !== this.props.children) {
+                              this.$el.trigger("chosen:updated");
+                    }
+          }
+
+          componentWillUnmount() {
+                    this.$el.off('change', this.handleChange);
+                    this.$el.chosen('destroy');
+          }
+          
+          handleChange(e) {
+                    this.props.onChange(e.target.value);
+          }
+
+          render() {
+                    return (
+                              <div>
+                                        <select className="Chosen-select" ref={el => this.el = el}>
+                                                  {this.props.children}
+                                        </select>
+                              </div>
+                    );
+          }
+}
+
 
 
 */ 
