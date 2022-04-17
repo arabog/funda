ba63931c18090cbc76d2264abae4af1eaa8f4416
arabambi <arababs2015@gmail.com>
2022-04-17 15:32:29 +0100

    react and oda lib

---
+++ React/react-funda/src/advancedGuides/OtherLibraries.jsx
@@ -0,0 +1,12 @@
+/*
+Integrating with Other Libraries
+React can be used in any web application. It can be embedded in 
+other applications and, with a little care, other applications can 
+be embedded in React. This guide will examine some of the more 
+common use cases, focusing on integration with jQuery and Backbone, 
+but the same ideas can be applied to integrating components with 
+any existing code.
+
+
+
+*/ 
