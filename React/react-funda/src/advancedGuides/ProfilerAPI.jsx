e6fe4d250c3718f82105f5fc78692bec8079e2a9
arabambi <arababs2015@gmail.com>
2022-04-19 13:43:04 +0100

    ProfilerAPI 4

--- React/react-funda/src/advancedGuides/ProfilerAPI.jsx
+++ React/react-funda/src/advancedGuides/ProfilerAPI.jsx
@@ -67,5 +67,26 @@ when necessary; each use adds some CPU and memory overhead to
 an application.
 
 
+-: onRender Callback
+The Profiler requires an onRender function as a prop. React calls 
+this function any time a component within the profiled tree “commits” 
+an update. It receives parameters describing what was rendered and 
+how long it took.
+
+function onRenderCallback(
+          id, // the "id" prop of the Profiler tree that has just committed
+          phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+          actualDuration, // time spent rendering the committed update
+          baseDuration, // estimated time to render the entire subtree without memoization
+          startTime, // when React began rendering this update
+          commitTime, // when React committed this update
+          interactions // the Set of interactions belonging to this update
+) {
+          // Aggregate or log render timings...
+}
+
+
+
+
 
 */
