735db24274616364484570dd6c8a9d867f158f30
arabambi <arababs2015@gmail.com>
2022-04-19 13:32:04 +0100

    ProfilerAPI 2

---
+++ React/react-funda/src/advancedGuides/ProfilerAPI.jsx
@@ -0,0 +1,14 @@
+/*
+Profiler API
+The Profiler measures how often a React application renders and 
+what the “cost” of rendering is. Its purpose is to help identify parts 
+of an application that are slow and may benefit from optimizations 
+such as memoization.
+
+Note:
+Profiling adds some additional overhead, so it is disabled in the 
+production build.
+
+
+
+*/
