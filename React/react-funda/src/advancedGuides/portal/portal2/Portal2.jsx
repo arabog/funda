b4ac7c6852339f51b91d55e2c5554ad21189ecbe
arabambi <arababs2015@gmail.com>
2022-04-19 12:24:05 +0100

    porter 5

---
+++ React/react-funda/src/advancedGuides/portal/portal2/Portal2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Portal2 = () => {
+          return (
+                    <div>
+                              Portal2
+                    </div>
+          )
+}
+
+
+export default Portal2
