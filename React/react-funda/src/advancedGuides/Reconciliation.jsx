4786e7c8f6f7879970f9081c6f0e533d5b3ec7b2
arabambi <arababs2015@gmail.com>
2022-04-20 11:28:32 +0100

    without ejs 4

--- React/react-funda/src/advancedGuides/Reconciliation.jsx
+++ React/react-funda/src/advancedGuides/Reconciliation.jsx
@@ -17,6 +17,83 @@ that changed. For example:
 When converting between these two elements, React knows to only 
 modify the color style, not the fontWeight.
 
+Recursing On Children
+By default, when recursing on the children of a DOM node, React just 
+iterates over both lists of children at the same time and generates a 
+mutation whenever there’s a difference.
+
+For example, when adding an element at the end of the children, 
+converting between these two trees works well:
+
+<ul>
+          <li>first</li>
+          <li>second</li>
+</ul>
+
+<ul>
+          <li>first</li>
+          <li>second</li>
+          <li>third</li>
+</ul>
+
+React will match the two <li>first</li> trees, match the two <li>second</li> 
+trees, and then insert the <li>third</li> tree.
+
+If you implement it naively, inserting an element at the beginning has worse 
+performance. For example, converting between these two trees works poorly:
+
+<ul>
+          <li>Duke</li>
+          <li>Villanova</li>
+</ul>
+
+<ul>
+          <li>Connecticut</li>
+          <li>Duke</li>
+          <li>Villanova</li>
+</ul>
+
+React will mutate every child instead of realizing it can keep the 
+<li>Duke</li> and <li>Villanova</li> subtrees intact. This 
+inefficiency can be a problem.
+
+Keys
+In order to solve this issue, React supports a key attribute. When children 
+have keys, React uses the key to match children in the original tree with 
+children in the subsequent tree. For example, adding a key to our inefficient 
+example above can make the tree conversion efficient:
+
+<ul>
+          <li key="2015">Duke</li>
+          <li key="2016">Villanova</li>
+</ul>
+
+<ul>
+          <li key="2014">Connecticut</li>
+          <li key="2015">Duke</li>
+          <li key="2016">Villanova</li>
+</ul>
+
+Now React knows that the element with key '2014' is the new one, and the 
+elements with the keys '2015' and '2016' have just moved.
+
+In practice, finding a key is usually not hard. The element you are going 
+to display may already have a unique ID, so the key can just come from 
+your data:
+
+<li key={item.id}>{item.name}</li>
+
+When that’s not the case, you can add a new ID property to your model or 
+hash some parts of the content to generate a key. The key only has to be 
+unique among its siblings, not globally unique.
+
+As a last resort, you can pass an item’s index in the array as a key. This 
+can work well if the items are never reordered, but reorders will be slow.
+
+
+
+
+
 
 
 
