41db686586ce299782cca4d1cf571f4304aa36d5
arabambi <arababs2015@gmail.com>
2022-04-21 13:54:45 +0100

    render props 2

--- React/react-funda/src/advancedGuides/RenderProps.jsx
+++ React/react-funda/src/advancedGuides/RenderProps.jsx
@@ -22,6 +22,15 @@ In this document, we’ll discuss why render props are useful,
 and how to write your own.
 
 
+-: Use Render Props for Cross-Cutting Concerns
+Components are the primary unit of code reuse in React, but 
+it’s not always obvious how to share the state or behavior 
+that one component encapsulates to other components that 
+need that same state.
+
+For example, the following component tracks the mouse 
+position in a web app:
+
 
 
 
