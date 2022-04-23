f5f85f562c0388d7d17cf74e91e7d4ded75c5318
HUGB <arababs2015@gmail.com>
2022-04-23 12:12:18 +0100

    useContext done

--- React/react-funda/src/advancedGuides/HookAPI/HookAPI.jsx
+++ React/react-funda/src/advancedGuides/HookAPI/HookAPI.jsx
@@ -74,10 +74,151 @@ instead, which will be executed only on the initial render:
 
 const [state, setState] = useState(() => {
           const initialState = someExpensiveComputation(props);
-          
+
           return initialState;
 });
 
+
+-: useEffect
+useEffect(didUpdate);
+
+By default, effects run after every completed render, but you can 
+choose to fire them only when certain values have changed.
+
+Cleaning up an effect
+Often, effects create resources that need to be cleaned up before 
+the component leaves the screen, such as a subscription or timer 
+ID. To do this, the function passed to useEffect may return a 
+clean-up function. For example, to create a subscription:
+
+useEffect(() => {
+          const subscription = props.source.subscribe();
+
+          return () => {
+                    // Clean up the subscription
+                    subscription.unsubscribe();
+          };
+});
+
+The clean-up function runs before the component is removed from 
+the UI to prevent memory leaks. Additionally, if a component 
+renders multiple times (as they typically do), the previous effect 
+is cleaned up before executing the next effect.
+
+Conditionally firing an effect
+The default behavior for effects is to fire the effect after every 
+completed render. That way an effect is always recreated if 
+one of its dependencies changes.
+
+However, this may be overkill in some cases, like the subscription 
+example from the previous section. We don’t need to create a new 
+subscription on every update, only if the source prop has changed.
+
+To implement this, pass a second argument to useEffect that is the 
+array of values that the effect depends on. Our updated example 
+now looks like this:
+
+useEffect(
+          () => {
+                    const subscription = props.source.subscribe();
+
+                    return () => {
+                              subscription.unsubscribe();
+                    };
+          }, [props.source],
+);
+
+Now the subscription will only be recreated when props.source 
+changes.
+
+Note
+If you use this optimization, make sure the array includes all values 
+from the component scope (such as props and state) that change 
+over time and that are used by the effect. Otherwise, your code 
+will reference stale values from previous renders.
+
+If you want to run an effect and clean it up only once (on mount and 
+unmount), you can pass an empty array ([]) as a second argument. 
+This tells React that your effect doesn’t depend on any values from 
+props or state, so it never needs to re-run. This isn’t handled as a 
+special case — it follows directly from how the dependencies array 
+always works.
+
+The array of dependencies is not passed as arguments to the effect 
+function. Conceptually, though, that’s what they represent: every 
+value referenced inside the effect function should also appear in 
+the dependencies array.
+
+
+-: useContext
+const value = useContext(MyContext);
+
+Accepts a context object (the value returned from React.createContext) 
+and returns the current context value for that context. The current context 
+value is determined by the value prop of the nearest <MyContext.Provider> 
+above the calling component in the tree.
+
+When the nearest <MyContext.Provider> above the component updates, 
+this Hook will trigger a rerender with the latest context value passed to that 
+MyContext provider. Even if an ancestor uses React.memo, a rerender 
+will still happen starting at the component itself using useContext.
+
+Don’t forget that the argument to useContext must be the context object itself:
+
+Correct: useContext(MyContext)
+
+Incorrect: useContext(MyContext.Consumer)
+Incorrect: useContext(MyContext.Provider)
+
+A component calling useContext will always re-render 
+when the context value changes. useContext(MyContext) 
+only lets you read the context and subscribe to its changes. 
+You still need a <MyContext.Provider> above in the tree 
+to provide the value for this context.
+
+Putting it together with Context.Provider:
+
+const themes = {
+          light: {
+                    foreground: "#000000",
+                    background: "#eeeeee"
+          },
+          
+          dark: {
+                    foreground: "#ffffff",
+                    background: "#222222"
+          }
+};
+
+const ThemeContext = React.createContext(themes.light);
+
+function App() {
+
+          return (
+                    <ThemeContext.Provider value={themes.dark}>       //ds will 1st be rendered
+                              <Toolbar />
+                    </ThemeContext.Provider>
+          );
+}
+
+function Toolbar(props) {
+          
+          return (
+                    <div>
+                              <ThemedButton />
+                    </div>
+                    );
+}
+
+function ThemedButton() {
+          const theme = useContext(ThemeContext);
+
+          return (
+                    <button style={{ background: theme.background, color: theme.foreground }}>
+                              I am styled by theme context!
+                    </button>
+          );
+}
 
 
 
