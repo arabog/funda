4379530866e4e477ba06466834fc37aaefa35d89
HUGB <arababs2015@gmail.com>
2022-04-24 12:59:21 +0100

    hooks updted 1

--- React/react-funda/src/hooks/HookAPI/AdditionalHooks.jsx
+++ React/react-funda/src/hooks/HookAPI/AdditionalHooks.jsx
@@ -126,6 +126,34 @@ equality to prevent unnecessary renders
 
 useCallback(fn, deps) is equivalent to useMemo(() => fn, deps).
 
+Note
+The array of dependencies is not passed as arguments to the callback. 
+Conceptually, though, that’s what they represent: every value referenced 
+inside the callback should also appear in the dependencies array. 
+
+
+-: useMemo
+const memoizedValue = useMemo(() => computeExpensiveValue(a, b), [a, b]);
+
+Returns a memoized value.
+Pass a “create” function and an array of dependencies. useMemo 
+will only recompute the memoized value when one of the dependencies 
+has changed. This optimization helps to avoid expensive calculations 
+on every render.
+
+
+Remember that the function passed to useMemo runs during rendering. 
+Don’t do anything there that you wouldn’t normally do while rendering. 
+For example, side effects belong in useEffect, not useMemo.
+
+If no array is provided, a new value will be computed on every render.
+
+You may rely on useMemo as a performance optimization, not as a 
+semantic guarantee.
+
+
+
+-: 
 
 
 
