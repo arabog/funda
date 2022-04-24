2e4ce3a472e5726b7ef1ab5c8bc106c1a69267cc
HUGB <arababs2015@gmail.com>
2022-04-24 13:48:45 +0100

    hooks updted 8

--- React/react-funda/src/hooks/HookAPI/AdditionalHooks.jsx
+++ React/react-funda/src/hooks/HookAPI/AdditionalHooks.jsx
@@ -414,6 +414,50 @@ content. This allows the user to continue interacting with the current
 content while rendering the update.
 
 
+-: useId
+const id = useId();
+useId is a hook for generating unique IDs that are stable across the server and client, while avoiding hydration mismatches.
+
+Note
+
+useId is not for generating keys in a list. Keys should be generated from your data.
+
+For a basic example, pass the id directly to the elements that need it:
+
+function Checkbox() {
+  const id = useId();
+  return (
+    <>
+      <label htmlFor={id}>Do you like React?</label>
+      <input id={id} type="checkbox" name="react"/>
+    </>
+  );
+};
+For multiple IDs in the same component, append a suffix using the same id:
+
+function NameFields() {
+          const id = useId();
+
+          return (
+                    <div>
+                              <label htmlFor={id + '-firstName'}>First Name</label>
+                              <div>
+                                        <input id={id + '-firstName'} type="text" />
+                              </div>
+
+                              <label htmlFor={id + '-lastName'}>Last Name</label>
+                              <div>
+                                        <input id={id + '-lastName'} type="text" />
+                              </div>
+                    </div>
+          );
+}
+
+Note:
+useId generates a string that includes the : token. This 
+helps ensure that the token is unique, but is not supported 
+in CSS selectors or APIs like querySelectorAll.
+
 
 
 
