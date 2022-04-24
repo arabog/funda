c4810e2e6d6bfb0b81a3129de3c68efdd9f056f1
HUGB <arababs2015@gmail.com>
2022-04-24 13:42:31 +0100

    hooks updted 6

--- React/react-funda/src/hooks/HookAPI/AdditionalHooks.jsx
+++ React/react-funda/src/hooks/HookAPI/AdditionalHooks.jsx
@@ -372,6 +372,47 @@ useDeferredValue, and it’s the same pattern you would
 use with similar hooks that use debouncing or throttling.
 
 
+-: useTransition
+const [isPending, startTransition] = useTransition();
+
+Returns a stateful value for the pending state of the transition, 
+and a function to start it.
+
+startTransition lets you mark updates in the provided callback 
+as transitions:
+
+startTransition(() => {
+          setCount(count + 1);
+})
+
+isPending indicates when a transition is active to show a 
+pending state:
+
+function App() {
+          const [isPending, startTransition] = useTransition();
+          const [count, setCount] = useState(0);
+          
+          function handleClick() {
+                    startTransition(() => {
+                              setCount(c => c + 1);
+                    })
+          }
+
+          return (
+                    <div>
+                              {isPending && <Spinner />}
+
+                              <button onClick={handleClick}>{count}</button>
+                    </div>
+          );
+}
+
+Note:
+Updates in a transition yield to more urgent updates such as clicks.
+Updates in a transitions will not show a fallback for re-suspended 
+content. This allows the user to continue interacting with the current 
+content while rendering the update.
+
 
 
 
