92ae4882be87b6a8abd9c04e6ee90e7062047112
HUGB <arababs2015@gmail.com>
2022-04-27 20:04:11 +0100

    nextjs 3 cont 9

--- nextjs/next-funda/chap/4.js
+++ nextjs/next-funda/chap/4.js
@@ -39,10 +39,70 @@ inside src/ , making our root directory a bit tidier.
 
 
 -: Organizing the components
+As for now, we will only discuss a folder structure that can 
+help us write and find configuration files, components, tests, 
+and styles with ease.
 
+We have different ways of setting up our folder structure. 
+We can start by separating components into three different 
+categories and then putting styles and tests in the same folder 
+for each component.
 
+To do that, create a new components/ folder inside our root 
+directory. Then, by moving inside it, create the following folders:
+mkdir components && cd components
+mkdir atoms
+mkdir molecules
+mkdir organisms
+mkdir templates
 
+As you may have noticed, we're following the atomic design 
+principle, where we want to divide our components into 
+different levels so as to organize our code base better. This
+is just a popular convention, and you're free to follow any 
+other approach for organizing your code.
 
+We will divide our components into four categories:
+• atoms : These are the most basic components that we 
+will ever write in our code base. Sometimes, they act 
+as a wrapper for standard HTML elements such as button , 
+input , and p , but we can also add animations, color palettes, 
+and so on, to this category of components.
+
+• molecules : These are a small group of atoms combined 
+to create slightly more complex structures with a minimum 
+of utility. The input atom and the label atom together can be 
+a straightforward example of what a molecule is.
+
+• organisms : Molecules and atoms combine to create 
+complex structures, such as a registration form, a footer, 
+and a carousel.
+
+• templates : We can think of templates as the skeleton of 
+our pages. Here, we decide where to put organisms, atoms, 
+and molecules together to create the final page that the 
+user will browse.
+
+If you're interested in learning more about atomic design, here's 
+a good article explaining it in detail: 
+https://bradfrost.com/blog/post/atomic-web-design .
+
+Now, let's pretend that we want to create a Button component. 
+When we create a new component, we often need at least three 
+different files: the component itself, its style, and a test file. 
+We can create those files by moving inside components/atoms/ 
+and then creating a new folder called Button/ . Once we create 
+this folder, we can move on to creating the components' files:
+          cd components/atoms/Button
+          touch index.js
+          touch button.test.js
+          touch button.styled.js # or style.module.css
+
+Organizing our components that way will help us a lot when we 
+need to search, update, or fix a given component. Let's say that 
+we spot a bug in production that involves our Button component. 
+We can easily find the component inside our code base, find its 
+test and styling files, and fix them.
 
 
 
