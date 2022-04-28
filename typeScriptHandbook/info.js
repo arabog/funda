8eb2c396a0cf383dc4e10239754104121a0bc70a
HUGB <arababs2015@gmail.com>
2022-04-28 04:52:18 +0100

    handbookTS 1

--- typeScriptHandbook/info.js
+++ typeScriptHandbook/info.js
@@ -167,6 +167,69 @@ be error prone.
 TypeScript also uses switch statements and equality checks like === , 
 !== , == , and != to narrow types. For example:
 
+function example(x: string | number, y: string | boolean) {
+          if(x === y) {
+                    we can now call any 'string' method on 'x' or 'y'
+                    x.toUpperCase();    //string
+                    y.toLowerCase();    //string
+          }else {
+                    console.log(x);     //x: string | number
+                    console.log(y);     //y: //string | boolean
+          }
+}
 
-cont on page 43
+When we checked that x and y are both equal in the above 
+example, TypeScript knew their types also had to be equal. 
+Since string is the only common type that both x and y could 
+take on, TypeScript knows that x and y must be a string in 
+the first branch.
+
+Checking against specific literal values (as opposed to variables) 
+works also. In our section about truthiness narrowing, we wrote 
+a printAll function which was error-prone because it accidentally 
+didn't handle empty strings properly. Instead we could have done 
+a specific check to block out null s, and TypeScript still correctly 
+removes null from the type of strs .
+
+function printAll(strs: string | string[] | null) {
+          if(str !== null) {
+                    if(typeof strs === 'object') {
+                              for(const s of strs) {        //strs: string[]
+                                        console.log(s)
+                              }
+                    }else if (typeof strs === 'string') {
+                              console.log(str)    //strs: string
+                    }
+          }
+}
+
+JavaScript's looser equality checks with == and != also 
+get narrowed correctly. If you're unfamiliar, checking 
+whether something == null actually not only checks 
+whether it is specifically the value null - it also checks 
+whether it's potentially undefined . The same applies to ==
+undefined : it checks whether a value is either null or undefined .
+
+interface Container {
+          value: number | null | undefined
+}
+
+function multipleValue(container: Container, factor: number) {
+          // remove both 'null' and 'undefined' from d type
+          if(container.value != null) {
+                    console.log(container.value) // value= Container.value: number
+
+                    // now we can safely multiply 'container.value'
+                    container.value * factor;
+          }
+}
+
+
+
+
+
+
+
+
+cont on page 53
 */ 
