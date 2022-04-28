b339e91001f679f764513dc4a3f5193666e3e5d9
HUGB <arababs2015@gmail.com>
2022-04-28 07:32:06 +0100

    handbookTS 7

--- typescript/typeScriptHandbook/interface.ts
+++ typescript/typeScriptHandbook/interface.ts
@@ -135,16 +135,165 @@ const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
 
 
 -: Discriminated unions
+For some motivation, let's imagine we're trying to encode shapes 
+like circles and squares. Circles keep track of their radiuses and 
+squares keep track of their side lengths. We'll use a field called
+kind to tell which shape we're dealing with. Here's a first attempt 
+at defining Shape .
+
+interface Shape {
+          kind: 'circle' | 'square';
+          radius?: number;
+          sideLength?; number;
+}
+
+Notice we're using a union of string literal types: "circle" 
+and "square" to tell us whether we should treat the shape 
+as a circle or square respectively. By using "circle" | "square" 
+instead of string, we can avoid misspelling issues.
+
+function handleShape(shape: Shape) {
+          // oops: This condition will always return 'false' since the 
+          // types '"circle" | "square"' and '"rect"' have no overlap
+          if(shape.kind === 'rect') {
+
+          }
+}
+
+We can write a getArea function that applies the right logic 
+based on if it's dealing with a circle or square. We'll first 
+try dealing with circles.
+
+function getArea(shape: Shape) {
+          return Math.PI * shape.radius ** 2;
+          // Object is possibly 'undefined'.
+}
+
+Under strictNullChecks that gives us an error - which is 
+appropriate since radius might not be defined. But what 
+if we perform the appropriate checks on the kind property?
+
+function getArea(shape: Shape) {
+          if (shape.kind === 'circle') {
+                    return Math.PI * shape.radius ** 2;
+                    // Object is possibly 'undefined'.
+          }
+}
+
+Hmm, TypeScript still doesn't know what to do here. We've 
+hit a point where we know more about our values than the 
+type checker does. We could try to use a non-null assertion 
+(a ! after shape.radius ) to say that radius is definitely present.
+
+function getArea(shape: Shape) {
+          if (shape.kind === "circle") {
+                    return Math.PI * shape.radius! ** 2;
+          }
+}
+
+
+The problem with this encoding of Shape is that the type-checker 
+doesn't have any way to know whether or not radius or sideLength 
+are present based on the kind property. We need to communicate 
+what we know to the type checker. With that in mind, let's take 
+another swing at defining Shape .
+
+interface Circle {
+          kind: 'circle';
+          radius: number;
+}
+
+interface Square {
+          kind:  'square';
+          sideLength: number;
+}
+
+type Shape = Circle | Square;
+
+Here, we've properly separated Shape out into two types 
+with different values for the kind property, but radius and 
+sideLength are declared as required properties in their 
+respective types.
+
+Let's see what happens here when we try to access the radius of a Shape .
+
+function getArea(shape: Shape) {
+          return Math.PI * shape.radius ** 2;
+          // Property 'radius' does not exist on type 'Shape'.
+          // Property 'radius' does not exist on type 'Square'.
+}
+
+Like with our first definition of Shape , this is still an error. 
+When radius was optional, we got an error (with 
+strictNullChecks enabled) because TypeScript couldn't tell 
+whether the property was present. Now that Shape is a union, 
+TypeScript is telling us that shape might be a Square , and 
+Square s don't have radius defined on them! Both 
+interpretations are correct, but only the union encoding of 
+Shape will cause an error regardless of how strictNullChecks 
+is configured.
+
+But what if we tried checking the kind property again?
+
+interface Circle {
+          kind: 'circle';
+          radius: number;
+}
+
+interface Square {
+          kind:  'square';
+          sideLength: number;
+}
+
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+          if (shape.kind === 'circle') {
+                    return Math.PI * shape.radius ** 2;
+          }
+}
+
+That got rid of the error! When every type in a union 
+contains a common property with literal types, TypeScript 
+considers that to be a discriminated union, and can narrow 
+out the members of the union.
+
+In this case, kind was that common property (which is what's 
+considered a discriminant property of Shape ). Checking 
+whether the kind property was "circle" got rid of every type 
+in Shape that didn't have a kind property with the type "circle" . 
+That narrowed shape down to the type Circle.
+
+The same checking works with switch statements as well. Now 
+we can try to write our complete getArea without any pesky ! 
+non-null assertions.
+
+interface Circle {
+          kind: 'circle';
+          radius: number;
+}
+
+interface Square {
+          kind:  'square';
+          sideLength: number;
+}
+
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+          switch(shape.kind) {
+                    case 'circle':
+                              return Math.PI * shape.radius ** 2;     //shape: Circle
+
+                    case 'square':
+                              return shape.sideLength ** 2;           ////shape: Square
+          }
+}
+
+
+-: The never type:
 
 
 
-
-
-
-
-
-
-cont on pg 53
+cont on pg 55
 */
-
-
