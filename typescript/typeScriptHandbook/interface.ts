170a7430d7fd324919e72c3af2994c0bfe28ebfb
HUGB <arababs2015@gmail.com>
2022-04-29 10:40:54 +0100

    handbookTS 2

--- typescript/typeScriptHandbook/interface.ts
+++ typescript/typeScriptHandbook/interface.ts
@@ -343,6 +343,106 @@ function getArea(shape: Shape) {
 }
 
 
+-: More on Functions
+They're also values, and just like other values, TypeScript 
+has many ways to describe how functions can be called. 
+Let's learn about how to write types that describe functions.
+
+Function Type Expressions
+The simplest way to describe a function is with a function 
+type expression. These types are syntactically similar to 
+arrow functions:
+
+function greeter(fn: (a: string) => void) {
+          fn('Hello, World')
+}
+
+function printToConsole(s: string) {
+          console.log(s)
+}
+
+greeter(printToConsole);
+
+The syntax (a: string) => void means "a function with one 
+parameter, named a , of type string, that doesn't have a 
+return value".
+
+Note that the parameter name is required. The function 
+type (string) => void means "a function with a parameter 
+named string of type any "!
+
+Of course, we can use a type alias to name a function type:
+
+type GreetFunction = (a: string) => void;
+
+function greeter(fn: GreetFunction) {}
+
+
+-: Call Signatures
+In JavaScript, functions can have properties in addition to being 
+callable. However, the function type expression syntax doesn't 
+allow for declaring properties. If we want to describe something 
+callable with properties, we can write a call signature in an object 
+type:
+
+type DescribableFunction = {
+          description: string;
+          (someArg: number): boolean;
+}
+
+function doSomething(fn: DescribableFunction) {
+          console.log(fn.description + " returned " + fn(6))
+}
+
+Note that the syntax is slightly different compared to a function 
+type expression - use : between the parameter list and the return 
+type rather than => .
+
+-: Construct Signatures
+JavaScript functions can also be invoked with the new operator. 
+TypeScript refers to these as constructors because they usually 
+create a new object. You can write a construct signature by
+adding the new keyword in front of a call signature:
+
+type SomeConstructor = {
+          new (s: string): SomeObject
+}
+
+function fn(ctor: SomeConstructor) {
+          return new ctor('Hello')
+}
+
+Some objects, like JavaScript's Date object, can be called with 
+or without new . You can combine call and construct signatures 
+in the same type arbitrarily:
+
+interface CallOrConstruct  {
+          new (s: string): Date;
+
+          (n?: number): number:
+}
+
+
+
+
+
+
+
+
+
+
+
 
 cont on pg 65
 */
+function greeter(fn: (a: string) => void) {
+          fn('Hello, World')
+}
+
+function printToConsole(s: string) {
+          console.log(s)
+}
+
+greeter(printToConsole);
+
+
