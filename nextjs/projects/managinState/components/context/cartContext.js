2e30fc9b86b9f4711b6a50563d53e687ba889d64
HUGB <arababs2015@gmail.com>
2022-04-30 11:36:58 +0100

    chap5 managing state 2

---
+++ nextjs/projects/managinState/components/context/cartContext.js
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+const ShoppingCartContext = createContext(
+          {
+                    items: {},
+
+                    setItems: () => null,
+          }
+)
+
+
+export default ShoppingCartContext;
--- nextjs/projects/managinState/pages/_app.js
+++ nextjs/projects/managinState/pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import React, { useState } from 'react';
+
+import Head from 'next/head';
+
+import Navbar from '../components/Navbar';
+
+import CartContext from '../components/context/cartContext'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const [items, setItems] = useState({});
+	
+
+	return (
+		<>
+			<Head>
+				<link
+					href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+					rel="stylesheet"
+				/>
+			</Head>
+
+			<CartContext.Provider value={{ items, setItems }}>
+				<Navbar />
+
+				<div className="w-9/12 m-auto pt-10">
+					<Component {...pageProps} />
+				</div>
+			</CartContext.Provider>
+		</>
+	);
 }
 
-export default MyApp
+
+export default MyApp;
