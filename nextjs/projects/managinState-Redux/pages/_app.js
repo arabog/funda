286dbbbd02de319fa6c6736a790769ff55882d3c
HUGB <arababs2015@gmail.com>
2022-04-30 16:13:35 +0100

    managingState-Redux 3 did not  work

--- nextjs/projects/managinState-Redux/pages/_app.js
+++ nextjs/projects/managinState-Redux/pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
 
+import { Provider } from 'react-redux'
+import { useStore } from '../redux/store'
+import Navbar from '../components/Navbar'
+import Head from 'next/head';
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const store = useStore(pageProps.initialReduxState);
+	
+
+	return (
+		<>
+			<Head>
+				<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+			</Head>
+
+			<Provider store={store}>
+				<Navbar />
+
+				<div className="w-9/12 m-auto pt-10">
+					<Component {...pageProps} />
+				</div>
+			</Provider>
+		</>
+	)
 }
 
 export default MyApp
