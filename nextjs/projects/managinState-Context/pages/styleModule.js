613f618a2d358f23617b38258fa0a7a3d7114d35
HUGB <arababs2015@gmail.com>
2022-05-01 19:30:02 +0100

    sass added

--- nextjs/projects/managinState-Context/pages/styleModule.js
+++ nextjs/projects/managinState-Context/pages/styleModule.js
@@ -7,6 +7,13 @@ export default function HomeStyle() {
           return (
                     <div className={styles.homepage}>
                               <h1 className={styles.title}> Welcome to the CSS Modules example </h1>
+
+                              <div>
+                                        <button className= {styles.button}>Click Me</button>
+                              </div>
                     </div>
           );
 }
+
+
+// cont on pg 164
