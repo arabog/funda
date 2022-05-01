0fa15eb8a930fa46d75e83b8f541d5cd860eff33
HUGB <arababs2015@gmail.com>
2022-05-01 20:39:48 +0100

    chap 7 2

--- nextjs/projects/UILib/pages/index.js
+++ nextjs/projects/UILib/pages/index.js
@@ -1,4 +1,4 @@
-import { Button, VStack } from '@chakra-ui/react'
+import { Button, useColorMode, VStack, Text } from '@chakra-ui/react'
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
@@ -6,6 +6,7 @@ import { Button, VStack } from '@chakra-ui/react'
 // @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^4 @chakra-ui/icons
 
 export default function Home() {
+	const { colorMode, toggleColorMode } = useColorMode();
 
 
 	return (
@@ -14,6 +15,17 @@ export default function Home() {
 			<Button backgroundColor='brand.200'>brand.200</Button>
 			<Button backgroundColor='brand.300'>brand.300</Button>
 			<Button backgroundColor='brand.400'>brand.400</Button>
+
+			<Text fontSize= '2xl' fontWeight='semibold' as='h2'>
+				Rendering in {colorMode}
+			</Text>
+
+			<Button
+				aria-label='UI Theme'
+				onClick={toggleColorMode}
+			>
+				Toggle {colorMode === 'light' ? 'dark' : 'light'} mode
+			</Button>
 		</VStack>
 	)
 }
