f80017ab4ae447e1377ca3ae3f9d506e796f0441
HUGB <arababs2015@gmail.com>
2022-05-01 19:55:02 +0100

    chap 6 done

--- nextjs/projects/managinState-Context/pages/styleModule.js
+++ nextjs/projects/managinState-Context/pages/styleModule.js
@@ -1,19 +1,29 @@
 import styles from '../styles/Home.module.css'
 
+import sasStyles from '../styles/Home.module.scss'
+
+console.log(sasStyles)
+
 
 export default function HomeStyle() {
 
 
           return (
-                    <div className={styles.homepage}>
-                              <h1 className={styles.title}> Welcome to the CSS Modules example </h1>
+                    <>
+                              <div className={styles.homepage}>
+                                        <h1 className={styles.title}> Welcome to the CSS Modules example </h1>
 
-                              <div>
-                                        <button className= {styles.button}>Click Me</button>
+                                        <div>
+                                                  <button className= {styles.button}>Click Me</button>
+                                        </div>
                               </div>
-                    </div>
+
+                              <div className={sasStyles.button}>
+                                        <h1> Welcome to the SASS Modules example </h1>
+                              </div>
+                    </>
           );
 }
 
 
-// cont on pg 164
+// cont on pg 165
