ba411e7ff6855a873d0cc3d72167610ef2566768
HUGB <arababs2015@gmail.com>
2022-05-03 00:15:18 +0100

    handbkTS 6

--- typescript/typeScriptHandbook/4.ts
+++ typescript/typeScriptHandbook/4.ts
@@ -69,8 +69,37 @@ explicitly pass in the type arguments when the compiler fails
 to infer the type, as may happen in more complex examples.
 
 
+-: Working with Generic Type Variables
+When you begin to use generics, you'll notice that when you 
+create generic functions like identity , the compiler will enforce 
+that you use any generically typed parameters in the body of
+the function correctly. That is, that you actually treat these 
+parameters as if they could be any and all types.
 
+What if we want to also log the length of the argument arg to the 
+console with each call? We might be tempted to write this:
 
+function loggingIdentity<Type>(arg: Type): Type {
+          console.log(arg.length);
+          
+          Property 'length' does nt exist on type 'Type'
+
+          return arg
+}
+
+When we do, the compiler will give us an error that we're using 
+the .length member of arg , but nowhere have we said that arg 
+has this member.
+
+Let's say that we've actually intended this function to work on 
+arrays of Type rather than Type directly. Since we're working 
+with arrays, the .length member should be available. We can
+describe this just like we would create arrays of other types:
+
+funtion loggingId<Type>(arg: Type[]): Type[] {
+          console.log(arg.length)
+          return arg;
+}
 
 
 cont on pg 105
