0d1e2577691f925906ea9e87f957404f0db40fe8
HUGB <arababs2015@gmail.com>
2022-05-04 21:46:04 +0100

    reactHB 9

--- typescript/typeScriptHandbook/5.tsx
+++ typescript/typeScriptHandbook/5.tsx
@@ -63,6 +63,58 @@ confusing trap of writing code you think is executing, but isn't:
 let shouldContinue: typeof msgbox("Are you sure you want to continue?");
 // ',' expected
 
+-: Indexed Access Types
+We can use an indexed access type to look up a specific 
+property on another type:
+
+ype Person = {age: number; name: string; alive: boolean};
+type Age = Person['age']
+
+The indexing type is itself a type, so we can use unions, keyof , or 
+other types entirely:
+
+type Person = {age: number; name: string; alive: boolean};
+type Age = Person['age']
+
+type T1 = Person['age' | 'name'];
+
+type T2 = Person[keyof Person]
+
+
+type AliveOrName = 'alive' | 'name'
+type T3 = Person[AliveOrName]
+
+You'll even see an error if you try to index a property that 
+doesn't exist:
+type I1 = Person["alve"];
+
+Another example of indexing with an arbitrary type is using 
+number to get the type of an array's elements. We can combine 
+this with typeof to conveniently capture the element type of an array
+literal
+
+const MyArray = [
+          { name: "Alice", age: 15 },
+          { name: "Bob", age: 23 },
+          { name: "Eve", age: 38 },
+]
+
+type Person = typeof MyArray[number]
+
+type Age = typeof Myrray[number]['age']
+Or:
+type Age2 = Person['age']
+
+You can only use types when indexing, meaning you can't 
+use a const to make a variable reference:
+
+const key = "age";
+type Age = Person[key];
+
+However, you can use a type alias for a similar style of refactor:
+
+type key = "age";
+type Age = Person[key];
 
 
 
