40f6ded14cdc606901cf63dbc69a1ee7e840db66
HUGB <arababs2015@gmail.com>
2022-05-05 08:36:13 +0100

    nextjs chap 8 customServer 2

---
+++ nextjs/projects/falsify-customServer/index.js
@@ -0,0 +1,21 @@
+const fastify = require('fastify')
+
+
+fastify
+          .register(require('fastify-nextjs'))
+          .after(() => {
+                    fastify.next('/');
+                    fastify.next('/about');
+                    fastify.next('/greet/:user');
+
+                    fastify.get('/contacts', (req, reply) => {
+                              reply
+                                        .type('html')
+                                        .send('<h1>Contacts page</h1>');
+                    });
+          })
+
+
+fastify.listen(3003, () => {
+          console.log('Server listening on http://localhost:3003')
+})
---
+++ nextjs/projects/falsify-customServer/pages/greet/[user].js
@@ -0,0 +1,17 @@
+export function getServerSideProps(req) {
+
+
+          return {
+                    props: { user: req.params.user },
+          };
+}
+
+export default function GreetUser({ user }) {
+
+          
+          return (
+                    <div>
+                              <h1>Hello {user}!</h1>
+                    </div>
+          );
+}
