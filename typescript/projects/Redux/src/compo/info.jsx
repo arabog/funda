9470ee32233cdc89e80d821e8ad4c39299759cd4
HUGB <arababs2015@gmail.com>
2022-05-08 20:05:12 +0100

    redux fund day1  1

---
+++ typescript/projects/Redux/src/compo/info.jsx
@@ -0,0 +1,13 @@
+/*
+-: What is Redux?
+Redux is a pattern and library for managing and updating application 
+state, using events called "actions". It serves as a centralized store for 
+state that needs to be used across your entire application,
+
+
+
+
+
+
+
+*/ 
