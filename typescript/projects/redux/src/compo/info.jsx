d4a3dff1ec505a8f8b2a7f2ca0fc94a411e11c13
HUGB <arababs2015@gmail.com>
2022-05-08 22:46:30 +0100

    redux funda 10

--- typescript/projects/redux/src/compo/info.jsx
+++ typescript/projects/redux/src/compo/info.jsx
@@ -229,6 +229,20 @@ const store = configureStore({ reducer: counterReducer })
 console.log(store.getState())           // {value: 0}
 
 
+-: Dispatch
+The Redux store has a method called dispatch. The only way to update 
+the state is to call store.dispatch() and pass in an action object. The 
+store will run its reducer function and save the new state value inside, 
+and we can call getState() to retrieve the updated value:
+
+store.dispatch({ type: 'counter/incremented' })
+
+console.log(store.getState())           // {value: 1}
+
+You can think of dispatching actions as "triggering an event" in the 
+application. Something happened, and we want the store to know 
+about it. Reducers act like event listeners, and when they hear an 
+action they are interested in, they update the state in response.
 
 
 
