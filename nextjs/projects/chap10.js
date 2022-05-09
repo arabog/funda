9cf3ae597bf061c9e443828e4a30b663435763aa
HUGB <arababs2015@gmail.com>
2022-05-09 22:03:32 +0100

    next js 1

---
+++ nextjs/projects/chap10.js
@@ -0,0 +1,87 @@
+/*
+Search engine bots make no difference; they only care 
+about website content and quality.
+
+
+There are specific SEO metrics that might be a bit out of 
+our control when developing the website. Domain authority, 
+referring domains, page impressions, click-through rate,
+and organic market share are just a few of them. Even
+though we are unlikely to improve those metrics during the 
+development process (as they are the product of good content
+management on the website), we should do our best to improve 
+whatever we can by coding the website.
+
+1. Creating an SEO-friendly routing structure:
+https://myblog.com/posts/how-to-deal-with-seo is a better URL,
+which tells us that on this page, we're talking about SEO and 
+how to deal with it. than this: https://myblog.com/posts/1
+
+2. Fill the pages with correct and complete metadata:
+This is essential data that we should always include in our pages, 
+with no exception. There are great libraries such as next-seo 
+( https://github.com/garmeeh/next-seo )
+
+3. Optimize your images: We've already talked about how 
+we can optimize images.
+
+4. Generate a proper sitemap: Once we're ready to deploy a website, 
+we can submit the sitemap to search engines to help them index our 
+content. A well-made sitemap is essential for any website as it allows 
+the creation of a neat, structured path for search engines to follow to 
+index the site
+eg: nextjs-sitemap-generator 
+https://github.com/IlusionDev/nextjs-sitemap-generator
+
+5. Use the correct HTML tags: Using semantic HTML tags to build 
+a website is essential as they tell the search engine bots how to index 
+the content in terms of priority and importance.
+
+-: Dealing with performance
+Performance and SEO are two important aspects of any web 
+application. Performance, in particular, can affect the SEO 
+score, as a lousy-performing website will lower the
+SEO score.
+
+Another thing that can potentially increase (or decrease) performance is 
+the deployment platform. For instance, if you're deploying a Next.js static 
+website to a CDN such as Cloudflare or AWS Cloudfront, you're likely to 
+get the best possible performance
+
+
+todo chap: 11
+-: A brief introduction to different deployment platforms
+While thinking about a new web application, we have many things 
+to consider. For example, how do we want to render its pages, which 
+styling method do we want to adopt, where does data come from, how 
+do we manage the application state, and where do we want to deploy 
+the application itself?
+
+Focusing on that last part, we could split one problem into two: where 
+do we want to deploy our application and how do we want to do it?
+In fact, most of the time, choosing a deployment platform also means 
+selecting a slightly different deployment method.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
