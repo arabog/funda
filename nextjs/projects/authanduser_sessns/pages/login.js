543b24765161cdb4905671e70027d5871758cf4d
HUGB <arababs2015@gmail.com>
2022-05-10 19:04:14 +0100

    auth resumed 4

---
+++ nextjs/projects/authanduser_sessns/pages/login.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import styles from '../styles/app.module.css';
+
+
+export default function Home() {
+          const router = useRouter();
+          const [loginError, setLoginError] = useState(null);
+
+          const handleSubmit = (event) => {
+                    event.preventDefault();
+
+                    const {email, password } = event.target.elemnts;
+
+                    setLoginError(null);
+
+                    handleLogin(email.value, password.value)
+                              .then(() => router.push('/protected-route'))
+                              .catch((err) => setLoginError(err.message))
+          }
+
+          return (
+                    <div className={styles.container} >
+                              <h1>Login</h1>
+
+                              <form className={styles.form} onSubmit={handleSubmit}>
+                                        <label htmlFor='email'>Email</label>
+                                        <input type="email" id="email" />
+
+                                        <label htmlFor="password">Password</label>
+                                        <input type="password" id="password" />
+
+                                        <button type="submit">Login</button>
+
+                                        {
+                                                  loginError && (
+                                                            <div className={styles.formError}>{loginError}</div>
+                                                  )
+                                        }
+                              </form>
+                    </div>
+          )
+}
+
+
+async function handleLogin(email, password) {
+          
+          const resp = await fetch('/api/login', {
+                    method: 'POST',
+
+                    headers: { 'Content-Type': 'application/json', },
+
+                    body: JSON.stringify({
+                              email,
+                              password,
+                    })
+          })
+
+          const data = await resp.json();
+
+          if (data.success) {
+                    return;
+          }
+
+          throw new Error('Wrong email or password');
+
+}
---
+++ nextjs/projects/authanduser_sessns/pages/protected-route.js
@@ -0,0 +1,13 @@
+import styles from '../styles/app.module.css
+'
+
+export default function ProtectedRoute() {
+
+          
+          return (
+                    <div className={styles.container}>
+                              <h1>Protected Route</h1>
+                              <p>You can't see me if not logged-in!</p>
+                    </div>
+          )
+}
