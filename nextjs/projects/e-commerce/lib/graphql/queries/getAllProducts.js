9d1d96c84753c48e94c5e6878eec2b1d9e79f221
HUGB <arababs2015@gmail.com>
2022-05-13 18:21:24 +0100

    next e-comm  3

---
+++ nextjs/projects/e-commerce/lib/graphql/queries/getAllProducts.js
@@ -0,0 +1,18 @@
+import {qql} from 'graphql-request'
+
+
+export default gql`
+          query GetAllProducs {
+          products {
+                    id
+                    name
+                    slug
+                    price
+                    images {
+                              id
+                              url
+                    }
+                    }
+          }
+`;
+
