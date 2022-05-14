58e89efe7df0616665315a543cd65571b5f562ca
HUGB <arababs2015@gmail.com>
2022-05-14 08:13:44 +0100

    home pg displayed  3

--- nextjs/projects/e-commerce/lib/graphql/queries/getAllProducts.js
+++ nextjs/projects/e-commerce/lib/graphql/queries/getAllProducts.js
@@ -8,6 +8,7 @@ export default gql `
                               name
                               slug
                               price
+                              
                               images {
                                         id
                                         url
---
+++ nextjs/projects/e-commerce/lib/graphql/queries/getProductDetail.js
@@ -0,0 +1,20 @@
+import { gql } from 'graphql-request';
+
+
+export default gql`
+          query GetProductBySlug($slug: String!) {
+                    products(where: { slug: $slug }) {
+                              id
+                              
+                              images(first: 1) {
+                                        id
+                                        url
+                              }
+
+                              name
+                              price
+                              slug
+                              description
+                    }
+          }
+`;
