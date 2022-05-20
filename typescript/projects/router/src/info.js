3bedf7163e9f13368b497fc34b6645bb93fb1fd4
HUGB <arababs2015@gmail.com>
2022-05-20 17:02:00 +0100

    contact form  1

---
+++ typescript/projects/router/src/info.js
@@ -0,0 +1,14 @@
+/*
+We'll learn how to build forms using controlled components in 
+React and TypeScript
+
+In React, the standard way to create a form is with what is 
+called controlled components. A controlled component has 
+its value synchronized with state in React.
+
+
+
+
+cont on pg 270
+
+*/ 
